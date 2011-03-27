9c3aca007c472d52973a9040dd59f4b3281d18c8
Damian Zaremba <damian@damianzaremba.co.uk>
2011-03-27 17:30:02 +0100

    People keep loosing it....

---
+++ cb_config.php
@@ -0,0 +1,17 @@
+<?PHP
+	$config = Array
+		(
+			'server' => '[2002:1828:83a7:0:210:5aff:fe83:df29]',
+			'port' => '6667',
+			'nick' => 'DaVinci',
+			'user' => 'ClueBot',
+			'gecos' => 'The Clueful Bot.',
+			'trigger' => '.',
+			'channels' => array ( '#clueirc', '#clueshells' ),
+			'mysqlhost' => '192.168.0.151',
+			'mysqlport' => 3306,
+			'mysqluser' => '',
+			'mysqlpass' => '',
+			'mysqldb' => 'cluebot'
+		);
+?>
