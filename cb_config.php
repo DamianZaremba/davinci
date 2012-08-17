5cc418e6825a866b88e769df7a3f7e64312790fa
Mantas Mikulėnas <grawity@gmail.com>
2012-08-17 17:16:31 +0300

    reconfigure

--- cb_config.php
+++ cb_config.php
@@ -1,14 +1,14 @@
 <?PHP
 $config = Array
 	(
-		'server' => "tcp://[2002:1828:83a7:0:210:5aff:fe83:df29]:6667",
+		'server' => "tcp://localhost:6667",
 		'nick' => 'DaVinci',
 		'user' => 'ClueBot',
 		'gecos' => 'The Clueful Bot.',
 		'irc_pass' => null,
 		'irc_mode' => "+Bp",
 		'trigger' => '.',
-		'channels' => array ( '#clueirc', '#clueshells' ),
+		'channels' => array('#clueirc', '#davinci_spam'),
 		'mysqlhost' => '192.168.0.151',
 		'mysqlport' => 3306,
 		'mysqluser' => '',
